7c258137973608d7e406372e5f212c3fe461a80b
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-10-06 10:21:52 -0500

    Correção de erros no AtendimentoList

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AgendaFormComponent } from './components/agenda-form/agenda-form.component';
 import { AgendaListComponent } from './components/agenda-list/agenda-list.component';
+import { AtendimentoListComponent } from './components/atendimento-list/atendimento-list.component';
+
 
 const routes: Routes = [
   { path: 'agenda', component: AgendaListComponent },
   { path: 'agenda/form', component: AgendaFormComponent },
-  { path: 'atendimento', component: AgendaListComponent }
+  { path: 'atendimento', component: AtendimentoListComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgendaListComponent } from './components/agenda-list/agenda-list.component';
-import { AtendimentoListComponent } from './components/atendimento-list/atendimento-list.component';
 import { AgendaFormComponent } from './components/agenda-form/agenda-form.component';
+import { AtendimentoListComponent } from './components/atendimento-list/atendimento-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AgendaListComponent,
-    AtendimentoListComponent,
-    AgendaFormComponent
+    AgendaFormComponent,
+    AtendimentoListComponent
   ],
   imports: [
     BrowserModule,
